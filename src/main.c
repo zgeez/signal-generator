44a33936c5e6892b5cc0d81bfa74cb524849cf29
Jamsekun <agentjames44@gmail.com>
2025-02-26 21:22:03 +0800

    SPI code test

--- src/main.c
+++ src/main.c
@@ -1,77 +1,88 @@
-// Wokwi Custom Chip - For docs and examples see:
-// https://docs.wokwi.com/chips-api/getting-started
+// Wokwi Custom SPI Chip Example
+//
+// This chip implements a simple ROT13 letter substitution cipher.
+// It receives a string over SPI, and returns the same string with
+// each alphabetic character replaced with its ROT13 substitution.
+//
+// For information and examples see:
+// https://link.wokwi.com/custom-chips-alpha
 //
 // SPDX-License-Identifier: MIT
-// Copyright 2023 
-//test fork
+// Copyright (C) 2022 Uri Shaked / wokwi.com
+
+
+
+//need ko burahin ung rot13
+//gumawa ng lalagyan ng hexadecimal code
 
 #include "wokwi-api.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 typedef struct {
-  // TODO: Put your chip variables here
-  pin_t inpin;
-  uart_dev_t uart0;
-  uint32_t tupnum_attr;
+  pin_t    cs_pin;
+  uint32_t spi;
+  uint8_t  spi_buffer[1];
 } chip_state_t;
 
+static void chip_pin_change(void *user_data, pin_t pin, uint32_t value);
+static void chip_spi_done(void *user_data, uint8_t *buffer, uint32_t count);
 
-void input_changed(void *user_data, pin_t pin, uint32_t value) {
-  // value will either be HIGH or LOW
-  chip_state_t* chip = (chip_state_t*)user_data;
-  //ipasa mo ung TUPM code
-  char buffer[20];
-  uint32_t randomNum = attr_read(chip->tupnum_attr);
-  sprintf(buffer, "TUPM-20-%d", randomNum);
-  // const char *data_out = "TUPM-20-" + randomNum;
-  uart_write(chip->uart0, (uint8_t *)buffer, strlen(buffer));
-
-
-}
-
-static void on_uart_rx_data(void *user_data, uint8_t byte);
-static void on_uart_write_done(void *user_data);
-
-
-void chip_init() {
+void chip_init(void) {
   chip_state_t *chip = malloc(sizeof(chip_state_t));
+  
+  chip->cs_pin = pin_init("CS", INPUT_PULLUP);
 
-  // pin_t inpin = pin_init("IN", INPUT_PULLDOWN);
-  chip->inpin = pin_init("IN", INPUT_PULLDOWN);
-  chip->tupnum_attr = attr_init("randomNum", 2669);
-
-  const pin_watch_config_t watch_input = {
-    .edge = RISING,
-    .pin_change = input_changed,
+  const pin_watch_config_t watch_config = {
+    .edge = BOTH,
+    .pin_change = chip_pin_change,
     .user_data = chip,
   };
-  pin_watch(chip->inpin, &watch_input);
+  pin_watch(chip->cs_pin, &watch_config);
 
-  const uart_config_t uart_config = {
-    .tx = pin_init("TX", INPUT_PULLUP),
-    .rx = pin_init("RX", INPUT),
-    .baud_rate = 115200,
-    .rx_data = on_uart_rx_data,
-    .write_done = on_uart_write_done,
+  const spi_config_t spi_config = {
+    .sck = pin_init("SCK", INPUT),
+    .miso = pin_init("MISO", INPUT),
+    .mosi = pin_init("MOSI", INPUT),
+    .done = chip_spi_done,
     .user_data = chip,
   };
-  chip->uart0 = uart_init(&uart_config);
-
-
-  // TODO: Initialize the chip, set up IO pins, create timers, etc.
-
-  printf("Hello from custom chip!\n");
-}
-
-static void on_uart_rx_data(void *user_data, uint8_t byte) {
-  chip_state_t *chip = (chip_state_t*)user_data;
-  printf("Incoming UART data: %d\n", byte);
+  chip->spi = spi_init(&spi_config);
+  
+  printf("SPI Chip initialized!\n");
 
 }
 
-static void on_uart_write_done(void *user_data) {
+
+void chip_pin_change(void *user_data, pin_t pin, uint32_t value) {
   chip_state_t *chip = (chip_state_t*)user_data;
-  printf("UART done\n");
+  // Handle CS pin logic
+  if (pin == chip->cs_pin) {
+    if (value == LOW) {
+      printf("SPI chip selected\n");
+      chip->spi_buffer[0] = ' '; // Some dummy data for the first character
+      spi_start(chip->spi, chip->spi_buffer, sizeof(chip->spi_buffer));
+    } else {
+      printf("SPI chip deselected\n");
+      spi_stop(chip->spi);
+    }
+  }
+}
+
+void chip_spi_done(void *user_data, uint8_t *buffer, uint32_t count) {
+  chip_state_t *chip = (chip_state_t*)user_data;
+  if (!count) {
+    // This means that we got here from spi_stop, and no data was received
+    return;
+  }
+
+  // Apply the ROT13 transformation, and store the result in the buffer.
+  // The result will be read back during the next SPI transfer.
+  //sending data happens here
+  buffer[0] = 0x4B;
+
+  if (pin_read(chip->cs_pin) == LOW) {
+    // Continue with the next character
+    spi_start(chip->spi, chip->spi_buffer, sizeof(chip->spi_buffer));
+  }
 }
